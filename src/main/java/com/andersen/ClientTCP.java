b1d741ac6a1849d024ef3490fd86295906550be5
Igor Martynovich <Ihor.Martynovich@gmail.com>
2017-01-25 13:02:47 +0200

    Commit from idea

--- src/main/java/com/andersen/ClientTCP.java
+++ src/main/java/com/andersen/ClientTCP.java
@@ -1,20 +1,34 @@
 package com.andersen;
 
+import java.io.*;
 import java.net.Socket;
 
 public class ClientTCP {
-	
+
 	public static void main(String[] args) {
+		Socket socket = null;
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		try {
-			Socket socket = new Socket("localhost", 3128);
-			String request = "Hello";
-			socket.getOutputStream().write(request.getBytes());
-			byte buf[] = new byte[64*1024];
-			int r = socket.getInputStream().read(buf);
-			String response = new String(buf, 0, r);
-			System.out.println(response);
+			socket = new Socket("localhost", 3128);
+			System.out.println("Enter message");
+			String line = reader.readLine();
+			BufferedReader bReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+			writer.println(line);
+			line = bReader.readLine();
+			System.out.println(line);
+
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (socket != null) {
+					socket.close();
+					socket = null;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
--- src/main/java/com/andersen/ServerTCP.java
+++ src/main/java/com/andersen/ServerTCP.java
@@ -1,30 +1,33 @@
 package com.andersen;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerTCP {
-	
+
 	public static void main(String[] args) {
-		try{
-			ServerSocket server = new ServerSocket(3128, 0 ,
-					InetAddress.getByName("localhost"));
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(3128, 0, InetAddress.getByName("localhost"));
 			System.out.println("Server is started");
 			Socket serverSocket = server.accept();
-			InputStream is = serverSocket.getInputStream();
-			OutputStream os = serverSocket.getOutputStream();
-			byte buf[] = new byte[64*1024];
-			int r = is.read(buf);
-			String request = new String(buf, 0, r);
-			String response = request + " servers answer";
-			os.write(response.getBytes());
-			serverSocket.close();
-		} catch(Exception e) {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+			PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(serverSocket.getOutputStream())), true);
+			String request = reader.readLine();
+			writer.println(request + " from server");
+		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			if (server != null) {
+				try {
+					server.close();
+					server = null;
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-		
 	}
 }
